66ae544456e02e4a7217a5d87c7e70e61035deb9
Najmul Hossain <phpnajmul@gmail.com>
2025-04-30 10:06:22 +0600

    I'm completed the Post CRUD

--- app/Http/Controllers/Backend/PostController.php
+++ app/Http/Controllers/Backend/PostController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -12,7 +14,9 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json([
+            'posts' => Post::all()
+        ]);
     }
 
     /**
@@ -28,7 +32,39 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'title'   => 'required|string|max:255',
+            'content' => 'required|string|max:255',
+        ]);
+
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        /*
+        Post::create([
+           'title'   => $request->input('title'),
+           'content' => $request->input('content'),
+        ]);
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Post has been created successfully!'
+        ]);
+        */
+
+        $insertPost          = new Post();
+        $insertPost->title   = $request->input('title');
+        $insertPost->content = $request->input('content');
+        $insertPost->save();
+
+        return response()->json([
+            'message' => 'Post has been created successfully!'
+        ]);
+
     }
 
     /**
@@ -44,7 +80,11 @@ class PostController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $postUpdate = Post::find($id);
+
+        return response()->json([
+            'editData' => $postUpdate
+        ]);
     }
 
     /**
@@ -52,7 +92,27 @@ class PostController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'title'   => 'required|string|max:255',
+            'content' => 'required|string|max:255',
+        ]);
+
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $post = Post::findOrFail($id);
+
+        $post->title   = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+
+        return response()->json([
+            'message' => 'Post has been updated successfully!',
+        ]);
     }
 
     /**
@@ -60,6 +120,11 @@ class PostController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $deletePost = Post::findOrFail($id);
+        $deletePost->delete();
+
+        return response()->json([
+            'message' => 'Post has been deleted successfully!'
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Backend\PostController;
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,3 +9,9 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+
+Route::get('/posts',[PostController::class,'index']);
+Route::post('/posts/store',[PostController::class,'store']);
+Route::get('/posts/edit/{id}',[PostController::class,'edit']);
+Route::patch('/posts/update/{id}', [PostController::class, 'update']);
+Route::delete('/posts/delete/{id}', [PostController::class, 'destroy']);
